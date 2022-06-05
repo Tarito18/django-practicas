578a010badd9482d41822dd7577294a280254114
Tarito18 <rensutcliff@gmail.com>
2022-06-05 18:08:49 -0300

    segunda parte proyecto

--- Practicas/ProyectoCoder/AppCoder/views.py
+++ Practicas/ProyectoCoder/AppCoder/views.py
@@ -11,3 +11,19 @@ def Curso(self):
     documentoDeTexto = f"--> Curso: {Curso.nombre}  Camada : {Curso.camada}" 
 
     return HttpResponse(documentoDeTexto)
+
+def inicio(request):
+    return render(request,"AppCoder/inicio.html")
+
+def cursos(request):
+    return render(request,"AppCoder/cursos.html")
+
+def profesores(request):
+    return render(request,"AppCoder/profesores.html")
+
+def estudiantes(request):
+    return render(request,"AppCoder/estudiantes.html")
+
+def entregables(request):
+    return render(request,"AppCoder/entregables.html")
+    
